f2ffb86f7af46ee2f2d7441f203ccc55a0ba4db5
Joshua Brinson <joshbrin2000@gmail.com>
2023-06-07 11:47:48 -0230

    added buttons, added grid layout, added event listeners

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function idCheck(id){
+    console.log(id)
+}
+
+
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        idCheck(button.id);
+    });
+});
