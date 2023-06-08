93f7ca61d2483bed1c855fe98238ba8dfa5e074d
Joshua Brinson <joshbrin2000@gmail.com>
2023-06-08 18:05:04 -0230

    uptodate

--- calculator.js
+++ calculator.js
@@ -8,48 +8,72 @@ pointToggle = true;
 function idCheck(id, value){
     //console.log(id);
     //console.log(value);
-    if (id === 'number'){
-        if (input === 0){
-            input = +value;
-            console.log(input);
-        }
-        else{
-            input += value;
-            input = Number(input);
-            console.log(input);
-        }
-        document.getElementById('output').value = input;
-    }
-    else if (id === 'zero'){
-        if (input !== 0){
-            input += value;
-            //input = Number(input); this prevents adding 0 after decimal
-            document.getElementById('output').value = input;
-            console.log(input);
-        }
-    }
-    else if (id === 'point'){
-        if (pointToggle === true){
-            input += value;
-            document.getElementById('output').value = input;
-            pointToggle = !pointToggle;
-            console.log(input);
-        }
-        else{
-            input += "";
-            console.log(input);
-        }
-    }
-    else if (id === 'posneg'){
-        input = -input;
-        document.getElementById('output').value = input;
-        console.log(input);
-    }
-    else if (id ==='AC'){
-        input = 0;
-        document.getElementById('output').value = input;
-    }
+    switch(id){
+        case 'number':
+            if (input === 0){
+                input = +value;
+                console.log(input);
+            }
+            else{
+                input += value;
+                input = Number(input);
+                console.log(input);
+            }
+            break;
+            
+        case 'zero':
+            if (input !== 0){
+                input += value;
+                //input = Number(input); this prevents adding 0 after decimal
+                console.log(input);
+            }
+            break;
 
+        case 'point':
+            if (pointToggle === true){
+                input += value;
+                pointToggle = !pointToggle;  //need to work flag reraising
+                console.log(input);
+            }
+            else{
+                input += "";
+                console.log(input);
+            }
+            break;
+
+        case 'posneg':
+            input = -input;
+            console.log(input);
+            break;
+
+        case 'AC':
+            input = 0;
+            pointToggle = !pointToggle;
+            break;  
+
+        case 'modulo':
+            break;
+        
+        case 'plus':
+            break;
+
+        case 'minus':
+            break;
+        
+        case 'times':
+            break;
+
+        case 'divide':
+            break;
+
+        case 'equals':
+            break;
+
+        default:
+            break;
+
+    }
+    document.getElementById('output').value = input;
 }
 
 
