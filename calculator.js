ac190b7ffeb4ed0e38623df1036d5f69182abe11
Joshua Brinson <joshbrin2000@gmail.com>
2023-06-08 14:13:29 -0230

    some basic button logic implemented and display screen changes on input

--- calculator.js
+++ calculator.js
@@ -1,5 +1,38 @@
-function idCheck(id){
-    console.log(id)
+let input = 0;
+let operator = 0;
+let operand = 0;
+let divisor = 0;
+let subtractor = 0;
+
+function idCheck(id, value){
+    //console.log(id);
+    //console.log(value);
+    if (id === 'number'){
+        if (input === 0){
+            input = +value;
+            //console.log(input);
+        }
+        else{
+            input += value;
+            input = Number(input);
+            //console.log(input);
+        }
+        document.getElementById('output').value = input;
+    }
+    else if (id === 'zero'){
+        if (input !== 0){
+            input += value;
+            input = Number(input);
+            //console.log(input);
+        }
+        document.getElementById('output').value = input;
+    }
+    else if (id == 'posneg'){
+        input = -input;
+        document.getElementById('output').value = input;
+        //console.log(input);
+    }
+
 }
 
 
@@ -7,6 +40,6 @@ function idCheck(id){
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        idCheck(button.className);
+        idCheck(button.className, button.value);
     });
 });
