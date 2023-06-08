58ebffeb725e794b141684c83946a88bd427bbfc
Joshua Brinson <joshbrin2000@gmail.com>
2023-06-08 14:23:25 -0230

    fixed decimal issue with adding 0 after decimal point

--- calculator.js
+++ calculator.js
@@ -3,6 +3,7 @@ let operator = 0;
 let operand = 0;
 let divisor = 0;
 let subtractor = 0;
+pointToggle = true;
 
 function idCheck(id, value){
     //console.log(id);
@@ -10,27 +11,43 @@ function idCheck(id, value){
     if (id === 'number'){
         if (input === 0){
             input = +value;
-            //console.log(input);
+            console.log(input);
         }
         else{
             input += value;
             input = Number(input);
-            //console.log(input);
+            console.log(input);
         }
         document.getElementById('output').value = input;
     }
     else if (id === 'zero'){
         if (input !== 0){
             input += value;
-            input = Number(input);
-            //console.log(input);
+            //input = Number(input); this prevents adding 0 after decimal
+            document.getElementById('output').value = input;
+            console.log(input);
         }
-        document.getElementById('output').value = input;
     }
-    else if (id == 'posneg'){
+    else if (id === 'point'){
+        if (pointToggle === true){
+            input += value;
+            document.getElementById('output').value = input;
+            pointToggle = !pointToggle;
+            console.log(input);
+        }
+        else{
+            input += "";
+            console.log(input);
+        }
+    }
+    else if (id === 'posneg'){
         input = -input;
         document.getElementById('output').value = input;
-        //console.log(input);
+        console.log(input);
+    }
+    else if (id ==='AC'){
+        input = 0;
+        document.getElementById('output').value = input;
     }
 
 }
